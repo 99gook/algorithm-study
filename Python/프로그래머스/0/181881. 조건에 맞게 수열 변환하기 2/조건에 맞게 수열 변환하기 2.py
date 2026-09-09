50c4510753dbc76413e821637d83741d80b16f6d
99gook <121601501+99gook@users.noreply.github.com>
2026-09-09 15:20:28 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 310.95 ms, Memory: 34.9 MB -BaekjoonHub

---
+++ Python/프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.py
@@ -0,0 +1,17 @@
+def solution(arr):
+    answer = 0
+    while True:
+        new_arr = []
+        for i in arr:
+            if i >= 50 and i % 2 == 0:
+                new_arr.append(i // 2)
+            elif i < 50 and i % 2 == 1:
+                new_arr.append(i * 2 + 1)
+            else:
+                new_arr.append(i)
+
+        if new_arr == arr:
+            return answer
+
+        arr = new_arr
+        answer += 1
