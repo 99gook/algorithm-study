bfb2b0c025e6d0abfcf3db51bab9b2dbd98d41b9
99gook <121601501+99gook@users.noreply.github.com>
2026-09-26 16:42:41 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 24.84 ms, Memory: 11.9 MB -BaekjoonHub

---
+++ Python/프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
@@ -0,0 +1,9 @@
+def solution(arr, k):
+    answer = []
+    for num in arr:
+        if num not in answer:
+            answer.append(num)
+        if len(answer) == k:
+            break
+    answer += [-1] * (k - len(answer))        
+    return answer
